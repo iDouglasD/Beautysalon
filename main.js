a4e56c9c05c000e8c313e1382e17cd73b3da4b2a
Douglas Duarte <douglasduarte.eng@gmail.com>
2021-07-12 09:48:44 -0300

    add all my files

---
+++ main.js
@@ -0,0 +1,110 @@
+/*==== OPEN AND CLOSE MENU WHEN CLICK ICON =====*/
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
+
+for (const element of toggle) {
+    element.addEventListener("click", function () {
+        nav.classList.toggle("show");
+    })
+}
+
+/*==== WHEN CLICK IN AN ITEM =====*/
+const links = document.querySelectorAll("nav ul li a")
+
+for (const link of links) {
+    link.addEventListener("click", function () {
+        nav.classList.remove("show")
+    })
+}
+
+/*==== CHANGE HEADER WHEN USE SCROLL =====*/
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+    if (window.scrollY >= navHeight) {
+        // scroll > header height
+        header.classList.add("scroll")
+
+    } else {
+        // scroll < header height
+        header.classList.remove("scroll")
+
+    }
+}
+
+/*==== TESTIMONIALS CAROUSEL SLIDER SWIPER =====*/
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+});
+
+/*==== ScrollReveal: Show elements when use scroll =====*/
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`#home .image, #home .text,
+ #about .image, #about .text,
+ #services header, #services .card,
+ #testimonials header, #testimonials .testimonials,
+ #contact .text, #contact .links,
+ footer .brand, footer .social`, {
+    interval: 100
+})
+
+/*==== Button back to top =====*/
+const backToTopButton = document.querySelector(".back-to-top")
+
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add("show")
+    } else {
+        backToTopButton.classList.remove("show")
+    }
+}
+
+/*==== Active menu in section =====*/
+const sections = document.querySelectorAll("main section[id]")
+
+function activateMenuAtCurrentSection() {
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute("id")
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add("active")
+
+        } else {
+
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove("active")
+        }
+    };
+}
+
+/*==== When scroll =====*/
+window.addEventListener("scroll", function () {
+    changeHeaderWhenScroll(),
+        backToTop()
+    activateMenuAtCurrentSection()
+})
